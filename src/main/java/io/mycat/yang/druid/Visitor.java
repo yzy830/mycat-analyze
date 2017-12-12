2abb6b14c3cddc5dde3ea46e60063c6b4957465a
yzy830 <yangzongyuan830@163.com>
2017-12-12 08:00:49 +0800

    修改mycat druid测试代码

--- src/main/java/io/mycat/yang/druid/Visitor.java
+++ src/main/java/io/mycat/yang/druid/Visitor.java
@@ -4,6 +4,7 @@ import io.mycat.route.parser.druid.MycatSchemaStatVisitor;
 
 import com.alibaba.druid.sql.ast.SQLStatement;
 import com.alibaba.druid.sql.dialect.mysql.parser.MySqlStatementParser;
+import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlSchemaStatVisitor;
 import com.alibaba.druid.sql.visitor.SchemaStatVisitor;
 
 /**
@@ -14,13 +15,37 @@ import com.alibaba.druid.sql.visitor.SchemaStatVisitor;
  */
 public class Visitor {
     public static void main(String[] args) {
-        String sql = "select e.eventId, e.eventKey, e.eventName, e.flag from "
-                + "event e join user u where eventId = ? and eventKey = ? and (eventName = ? or eventName = ?) "
-                + "and u.user_id = 1 and e.event_time = ? group by u.user_id order by eventName";
-        MySqlStatementParser parser = new MySqlStatementParser(sql); 
-        SQLStatement statement = parser.parseStatement(); 
-        MycatSchemaStatVisitor visitor = new MycatSchemaStatVisitor(); 
+    	String sql = "select o.order_id as orderId, max(o.order_sum) as orderSum, upper(uv.user_name) userName from t_d_order o "
+                + "join (select u.user_id, u.user_name,u.user_gender from t_d_user u join t_d_user_detail ud on u.user_id = ud.user_id) uv on o.user_id = uv.user_id "
+                + "where o.order_sum > 100 and uv.user_name in ('a', 'b') group by o.user_id having max(o.order_sum) > 200 order by uv.gender desc limit 0,10";
         
+        MySqlStatementParser parser = new MySqlStatementParser(sql);
+        
+        SQLStatement statement = parser.parseStatement();
+        
+        MySqlSchemaStatVisitor visitor = new MySqlSchemaStatVisitor();
         statement.accept(visitor);
+        
+        System.out.println("columns = " + visitor.getColumns());
+        System.out.println("conditions = " + visitor.getConditions());
+        System.out.println("aggregates = " + visitor.getAggregateFunctions());
+        System.out.println("funtions = " + visitor.getFunctions());
+        System.out.println("groupByColumns = " + visitor.getGroupByColumns());
+        System.out.println("orderByColumns = " + visitor.getOrderByColumns());
+        System.out.println("parameters = " + visitor.getParameters());
+        System.out.println("relations = " + visitor.getRelationships());
+        System.out.println("tables = " + visitor.getTables());
+        System.out.println("currentTable = " + visitor.getCurrentTable());
+        System.out.println("aliasMap = " + visitor.getAliasMap());
+        System.out.println("variants = " + visitor.getVariants());
+    	
+//        String sql = "select e.eventId, e.eventKey, e.eventName, e.flag from "
+//                + "event e join user u where eventId = ? and eventKey = ? and (eventName = ? or eventName = ?) "
+//                + "and u.user_id = 1 and e.event_time = ? group by u.user_id order by eventName";
+//        MySqlStatementParser parser = new MySqlStatementParser(sql); 
+//        SQLStatement statement = parser.parseStatement(); 
+//        MycatSchemaStatVisitor visitor = new MycatSchemaStatVisitor(); 
+//        
+//        statement.accept(visitor);
     }
 }
